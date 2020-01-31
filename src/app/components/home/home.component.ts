e9d841ca0251a0e3c7a93de63d976c410a6dedf3
Manish Malepati <mr.malepati@gmail.com>
2020-01-31 23:48:41 +0530

    Heroku Readying

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { BeerService } from '../../services/beer.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  beers = [{ product_id:"beer1", title:"Lager", desc:"Lagers are a typical entry point into beer for new drinkers. Made with bottom fermenting yeast that has a lower tolerance to alcohol.", src:"../../../assets/images/baltika7.jpg", fav:false }, { product_id:"beer2", title:"India Pale Ales", desc:"India Pale Ales (IPAs), which encompass numerous styles of beer, get their characteristics from citrus flavors.", src:"../../../assets/images/beer2.jpg", fav:false }, { product_id:"beer3", title:"Beer3", desc:"Beer3, which encompass numerous styles of beer, get their characteristics from citrus flavors.", src:"../../../assets/images/beer3.jpg", fav:false }, { product_id:"beer4", title:"Beer4", desc:"Beer4, which encompass numerous styles of beer, get their characteristics from citrus flavors.", src:"../../../assets/images/baltika7.jpg", fav:false }, { product_id:"beer5", title:"Beer5", desc:"Beer5, which encompass numerous styles of beer, get their characteristics from citrus flavors.", src:"../../../assets/images/beer2.jpg", fav:false }, { product_id:"beer6", title:"Beer6", desc:"Beer6, which encompass numerous styles of beer, get their characteristics from citrus flavors.", src:"../../../assets/images/beer3.jpg", fav:false }];
+  beers = [];
   search_text: string = "";
 
   constructor(private beerService: BeerService) { }
